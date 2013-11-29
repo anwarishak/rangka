a640b35e3cde5867cb51b35f9aef8e7b4f212105
Anwar Ishak <anwarishak@gmail.com>
2013-11-29 02:01:57 +0000

    The beginnings of the routing startegy

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
 
-redirect_url = isset($_SERVER['REDIRECT_URL']) ? trim($_SERVER['REDIRECT_URL'], '/ ') : '';
+$path = isset($_SERVER['REDIRECT_URL']) ? trim($_SERVER['REDIRECT_URL'], '/ ') : '';
+$path_parts = explode('/', $path);
+
+$main_path = empty($path_parts[0]) ? 'home' : $path_parts[0];
+$secondary_path = empty($path_parts[1]) ? '' : $path_parts[1];
+$tertiary_path = empty($path_parts[2]) ? '' : $path_parts[2];
+$quarternary_path = empty($path_parts[3]) ? '' : $path_parts[3];
+
+$controller = controller::get_instance($main_path);
+
+
+
+
+
+
+
+
+
+
+// Utility functions
+
+function __autoload($class_name)
+{
+  $rangka_file = 'rangka/'.$class_name.'.php';
+  include $rangka_file;
+}
---
+++ rangka/controller.php
@@ -0,0 +1,17 @@
+<?php
+
+abstract class controller
+{
+  public function __constuct() 
+  {
+
+  }
+
+  public static function get_instance($name)
+  {
+    // if controller class exists
+    //   return the cotroller object
+    // else
+    //   redirect to 404
+  }
+}
