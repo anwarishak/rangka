d61ef8a24b74e6608a39a151900171c4ea989b18
Anwar Ishak <anwarishak@gmail.com>
2013-12-16 00:09:00 +0000

    Sign in page/form

--- rangka/templates/sign_in.php
+++ rangka/templates/sign_in.php
@@ -4,20 +4,35 @@ include 'header.php';
 
 ?>
 
-<h1>Sign in</h1>
+<div class="page-header">
+  <h1>Sign in</h1>
+</div>
 
-<form method="post" action="/sign-in">
-  <p>
-    <label for="email">Username / email address</label>
-    <input type="email" id="email" name="email" value="" maxlength="128" class="text" />
-  </p>
-  <p>
-    <label for="password">Password</label>
-    <input type="password" name="password" class="text" />
-  </p>
-  <p>
-    <input type="submit" value="Sign in" />
-  </p>
+<form class="form-horizontal" role="form" method="post" action="/sign-in">
+  <div class="form-group">
+    <label for="email" class="col-md-3 control-label">Username / email address</label>
+    <div class="col-md-4">
+      <input type="email" id="email" name="email" class="form-control" value="" maxlength="128" />
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="password" class="col-md-3 control-label">Password</label>
+    <div class="col-md-4">
+      <input type="password" name="password" class="form-control" />
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-md-offset-3 col-md-4">
+      <div class="checkbox">
+        <label><input type="checkbox" id="remember" name="remember" value="1"> Remember me for 2 weeks</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-md-offset-3 col-md-4">
+      <button type="submit" class="btn btn-default">Sign in</button>
+    </div>
+  </div>
 </form>
 
 <?php
