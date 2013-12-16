6d323b33747c975472b86dc1bdfa7a2c9062fa9e
Anwar Ishak <anwarishak@gmail.com>
2013-12-16 00:22:15 +0000

    Improvements to sign-in form presentation

--- rangka/templates/sign_in.php
+++ rangka/templates/sign_in.php
@@ -8,32 +8,44 @@ include 'header.php';
   <h1>Sign in</h1>
 </div>
 
-<form class="form-horizontal" role="form" method="post" action="/sign-in">
-  <div class="form-group">
-    <label for="email" class="col-md-3 control-label">Username / email address</label>
-    <div class="col-md-4">
-      <input type="email" id="email" name="email" class="form-control" value="" maxlength="128" />
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="password" class="col-md-3 control-label">Password</label>
-    <div class="col-md-4">
-      <input type="password" name="password" class="form-control" />
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-md-offset-3 col-md-4">
-      <div class="checkbox">
-        <label><input type="checkbox" id="remember" name="remember" value="1"> Remember me for 2 weeks</label>
+<div class="row">
+  <div class="col-md-offset-3 col-md-6">
+    <div class="panel panel-default">
+      <div class="panel-body">
+
+        <!--div class="alert alert-danger">Incorrect sign in details</div-->
+  
+        <form class="form-horizontal" role="form" method="post" action="/sign-in">
+          <div class="form-group">
+            <label for="email" class="col-md-4 control-label">Username</label>
+            <div class="col-md-8">
+              <input type="email" id="email" name="email" class="form-control" value="" maxlength="128" />
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="password" class="col-md-4 control-label">Password</label>
+            <div class="col-md-8">
+              <input type="password" name="password" class="form-control" />
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-md-offset-4 col-md-8">
+              <div class="checkbox">
+                <label><input type="checkbox" id="remember" name="remember" value="1"> Remember me for 2 weeks</label>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-md-offset-4 col-md-8">
+              <button type="submit" class="btn btn-default">Sign in</button>
+            </div>
+          </div>
+        </form>
+
       </div>
     </div>
   </div>
-  <div class="form-group">
-    <div class="col-md-offset-3 col-md-4">
-      <button type="submit" class="btn btn-default">Sign in</button>
-    </div>
-  </div>
-</form>
+</div>
 
 <?php
 
