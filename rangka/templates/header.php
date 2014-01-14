5934efcdc546c867c3db695c6140e075d8e6a5c8
Anwar Ishak <anwarishak@gmail.com>
2014-01-14 23:35:08 +0000

    Style changes

--- rangka/templates/header.php
+++ rangka/templates/header.php
@@ -13,45 +13,41 @@
 </head>
 <body>
 
-  <div class="navbar-wrapper">
-    <div class="container">
-      <nav class="navbar navbar-inverse" role="navigation">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="/">Rangka</a>
-        </div>
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <!--ul class="nav navbar-nav">
-            <li class="active"><a href="#">Link</a></li>
-            <li><a href="#">Link</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li><a href="#">Separated link</a></li>
-                <li class="divider"></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul-->
-          <ul class="nav navbar-nav navbar-right">
-            <li><button type="button" class="btn btn-default navbar-btn">Register</button></li>
-            <li class="navbar-text">or</li>
-            <li<?php if ($main_path == 'sign-in'): ?> class="active"<?php endif; ?>><button type="button" class="btn btn-default navbar-btn">Sign in</button></li>
-
-          </ul>
-        </div>
-      </nav>
+<div class="container navbar-container">
+  <nav class="navbar navbar-inverse" role="navigation">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/">Rangka</a>
     </div>
-  </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <!--ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul-->
+      <div class="nav navbar-right">
+        <p class="navbar-text"><a href="" class="navbar-link">Sign in</a> or</p>
+        <button type="button" class="btn btn-danger navbar-btn btn-sm">Create an account</button>
+      </div>
+    </div>
+  </nav>
+</div>
 
   <div class="container">
 
