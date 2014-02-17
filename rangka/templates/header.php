d2be0d7a9e33ad9e7917afb82e5309e3667ea59f
Anwar Ishak <anwarishak@gmail.com>
2014-02-17 00:15:31 +0000

    Style changes to the nav

--- rangka/templates/header.php
+++ rangka/templates/header.php
@@ -3,8 +3,8 @@
 <head>
   <title>Rangka</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="/css/bootstrap.min.css" rel="stylesheet">
-  <!--link href="/css/bootstrap-theme.min.css" rel="stylesheet"-->
+  <!--link href="/css/bootstrap.min.css" rel="stylesheet"-->
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
   <link href="/css/rangka.css" rel="stylesheet">
   <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -13,38 +13,51 @@
 </head>
 <body>
 
-<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+<nav class="navbar navbar-default navbar-static-top" role="navigation">
   <div class="container">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <!--a class="navbar-brand" href="/">Rangka</a-->
+      <a class="navbar-brand" href="/">Rangka</a>
     </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="/">Home</a></li>
-        <!--li><a href="#">Link</a></li-->
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Settings <b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <li><a href="/users">Users</a></li>
-            <li class="divider"></li>
-            <li><a href="/about">About Rangka</a></li>
-          </ul>
-        </li>
+        <li><a href="/">Dashboard</a></li>
+        <li><a href="/">Reports</a></li>
+        <li class="active"><a href="/users">Users</a></li>
       </ul>
       <div class="nav navbar-right navbar-text">
-        <a href="/sign-in" class="navbar-link">Sign in</a> or
-        <a href="/create-account" class="navbar-link">Create an account</a>
+        Signed in as <a href="/profile">Anwar Ishak</a>. <a href="/sign-out">Sign out</a>.
+        <!--a href="">Sign in</a> or <a href="">create an account</a-->
       </div>
-    </div>
-  </div>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
 </nav>
 
-  <div class="container">
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <ul class="nav nav-pills">
+        <li class="active"><a href="/users">Users</a></li>
+        <li><a href="/roles">Roles</a></li>
+        <li><a href="/permissions">Permissions</a></li>
+      </ul>
+    </div>
+    <!--div class="col-md-6 text-right">
+      <form class="form-inline" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+      </form>
+    </div-->
+</div>
+
+<div class="container">
 
 
